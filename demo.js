29078736f4b623c78a1b6cffa3d003682d0c140c
samhamm <samhamm@gmail.com>
2015-10-01 13:45:40 -0700

    changes through class time on thur 10-1

--- demo.js
+++ demo.js
@@ -1,23 +1,36 @@
-var question1 = prompt("Was I born in South Dakota");
+var one  = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
 
-if (question1.toUpperCase() === 'NO') {
-  alert('Correct! Nice job.');
-} else {
-  alert('No, you silly goose, I was born in Germany.');
+// I declare....
+function ques1() {
+  var question1 = prompt("Was I born in South Dakota?");
+  if (question1.toUpperCase() === 'NO' ||  question1.toUpperCase() === 'N') {
+    one.innerHTML = 'Correct! Nice job.';
+  } else {
+    one.innerHTML = 'No, you silly goose, I was born in Germany.';
+  }
 }
 
-var question2 = prompt("Do I have a dog?");
-
-if (question2.toUpperCase() === 'YES') {
-  alert('Indeed I do! I have two: Demi and Parker.');
-} else {
-  alert('Wrong! Have you not noticed the dog on my computer desktop?');
+function ques2() {
+  var question2 = prompt("Do I have a dog?");
+  if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
+    two.innerHTML = 'Indeed I do! I have two: Demi and Parker.';
+  } else {
+    two.innerHTML = 'Wrong! Have you not noticed the dog on my computer desktop?';
+  }
 }
 
-var question3 = prompt("Am I left-handed?");
-
-if (question3.toUpperCase() === 'YES') {
-  alert('Right! Wait... I mean, left... yes, I\'m left-handed.');
-} else {
-  alert('Nope. You\'ve not noticed when I have been writing on the whiteboard?');
+function ques3() {
+  var question3 = prompt("Am I left-handed?");
+  if (question3.toUpperCase() === 'YES' || question3.toUpperCase() === 'Y') {
+    three.innerHTML = 'Right! Wait... I mean, left... yes, I\'m left-handed.';
+  } else {
+    three.innerHTML = 'Nope. You\'ve not noticed when I have been writing on the whiteboard?';
+  }
 }
+
+// I call.
+ques1();
+ques2();
+ques3();
---
+++ functions.js
@@ -0,0 +1,15 @@
+function sayHello() {
+  alert('Hello');
+}
+
+function sayHelloAgain() {
+  alert('Hello');
+  console.log('Hello again');
+}
+
+function sayHelloTo(someone) {
+  alert('Hello ' + someone);
+  console.log(someone + ' just got told hello');
+}
+
+sayHelloTo('Bob');
